3aa4523ab1435675ce0f45277a9fa39c77832c57
zhanghao <zhanghao.326159@1613.com>
2015-08-06 23:02:16 +0800

    图片墙

--- MiaoTu/src/main/java/com/miaotu/activity/BBSTopicDetailActivity.java
+++ MiaoTu/src/main/java/com/miaotu/activity/BBSTopicDetailActivity.java
@@ -201,9 +201,9 @@ public class BBSTopicDetailActivity extends BaseActivity implements View.OnClick
             tvEmotion = (TextView) view.findViewById(R.id.tv_emotion);
             tvTipComment = (TextView) view.findViewById(R.id.tv_tip_comment);
             tvAge.setText(topic.getAge() + "岁");
-            if ("15".equals(topic.getAge())){
+            if ("15".equals(topic.getAge())|| Integer.parseInt(topic.getAge())<16){
                 tvAge.setText("<16岁");
-            }else if ("61".equals(topic.getAge())){
+            }else if ("61".equals(topic.getAge())|| Integer.parseInt(topic.getAge())>60){
                 tvAge.setText(">60岁");
             }
             if (StringUtil.isBlank(topic.getEmotion())){
